bcce4560d14a4ad3d5f4732a0f391b473ed932be
Matheus R. Brunelli <matheus.brunelli@gmail.com>
2021-08-08 12:15:50 -0300

    fix: fix all envs

--- handler.js
+++ handler.js
@@ -3,7 +3,7 @@ const { SQS } = require("aws-sdk");
 const sqs = new SQS()
 
 exports.producer = async (event) => {
-  console.log(event)
+  console.log(process.env.QUEUE_URL)
 }
 
 exports.consumer = async (event) => {
