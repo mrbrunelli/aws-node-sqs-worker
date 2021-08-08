2913462055f6e47eb33fc3e1a2fadc36a0623a3b
Matheus R. Brunelli <matheus.brunelli@gmail.com>
2021-08-08 10:48:50 -0300

    refactor: remove unused configs

--- handler.js
+++ handler.js
@@ -1,61 +1,11 @@
 const { SQS } = require("aws-sdk");
 
-const sqs = new SQS();
+const sqs = new SQS()
 
-const producer = async (event) => {
-  let statusCode = 200;
-  let message;
+exports.producer = async (event) => {
+  console.log(event)
+}
 
-  if (!event.body) {
-    return {
-      statusCode: 400,
-      body: JSON.stringify({
-        message: "No body was found",
-      }),
-    };
-  }
-
-  try {
-    await sqs
-      .sendMessage({
-        QueueUrl: process.env.QUEUE_URL,
-        MessageBody: event.body,
-        MessageAttributes: {
-          AttributeName: {
-            StringValue: "Attribute Value",
-            DataType: "String",
-          },
-        },
-      })
-      .promise();
-
-    message = "Message accepted!";
-  } catch (error) {
-    console.log(error);
-    message = error;
-    statusCode = 500;
-  }
-
-  return {
-    statusCode,
-    body: JSON.stringify({
-      message,
-    }),
-  };
-};
-
-const consumer = async (event) => {
-  for (const record of event.Records) {
-    const messageAttributes = record.messageAttributes;
-    console.log(
-      "Message Attributte: ",
-      messageAttributes.AttributeName.stringValue
-    );
-    console.log("Message Body: ", record.body);
-  }
-};
-
-module.exports = {
-  producer,
-  consumer,
-};
+exports.consumer = async (event) => {
+  console.log(event)
+}
